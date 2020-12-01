23fefc2e1a7c1f362c4c50fc3a5c763363e3489a
MonzonIker <monzonyt@gmail.com>
2020-12-01 10:49:34 +0100

    ariketa 4

--- angular/ariketa4/js/jsFunctions.js
+++ angular/ariketa4/js/jsFunctions.js
@@ -2,19 +2,19 @@ let myApp = angular.module('myApp', []);
 
 myApp.controller('myController', function ($scope) {
     $scope.ver = false
-    $scope.ListaSeleccionados = [];
+    $scope.lista = [];
 
-    $scope.load = function () {
-        $scope.mySelect;
+    $scope.cargar = function () {
+        $scope.select;
 
-        if ($scope.mySelect != undefined) {
-            $scope.ListaSeleccionados.push({ nombre: $scope.mySelect, curso: 1 });
+        if ($scope.select != undefined) {
+            $scope.lista.push({ nombre: $scope.select, curso: 1 });
             $scope.ver = true;
         }
     }
 
-    $scope.clean = function () {
-        $scope.ListaSeleccionados = [];
+    $scope.limpiar = function () {
+        $scope.lista = [];
         $scope.ver = false
     }
 });
